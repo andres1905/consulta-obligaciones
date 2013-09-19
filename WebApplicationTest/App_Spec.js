4fadc2dd29ad10bdc8f5b6a7835896a7e7515ece
txau <jaumec@gmail.com>
2013-09-19 12:19:05 -0500

    Added Angular.js and Karma

---
+++ WebApplicationTest/App_Spec.js
@@ -0,0 +1,13 @@
+﻿'use strict';
+
+describe('App does something', function () {
+
+    it('should have sex with Joans mother', function () {
+        expect(true).toBe(true);
+    });
+
+    it('should fail', function () {
+        expect(true).toBe(true);
+    });
+});
+
